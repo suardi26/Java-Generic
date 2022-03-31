82a93a72ace7ee1eb2004a7664f9c258b12c9fcd
suardi26 <suardi260696@gmail.com>
2022-03-31 16:43:05 +0800

    Update Project Java

--- Type Erasure/src/com/practice/Main.java
+++ Type Erasure/src/com/practice/Main.java
@@ -3,7 +3,8 @@ package com.practice;
     - Type erasure adalah proses pengecekan generic pada saat compile program, dan menghiraukan pengecekan pada saat program berjalan
       (runtime).
     - Type erasure menjadikan informasi generic yang dibuat akan hilang ketika kode program telah di compile menjadi binary file.
-    - Compiler akan mengubah generic parameter type menjadi tipe Object di java.
+    - Compiler akan mengubah generic parameter type menjadi tipe data Object di java, dimana tipe data Object
+      dapat di diisi seluruh class pada java karena tipe data Object adalah superclass dari seluruh class di java.
     - Informasi generic akan hilang ketika sudah menjadi binary file, maka dari itu konversi tipe data generic akan berbahaya jika
       dilakukan secara tidak bijak.
 */
